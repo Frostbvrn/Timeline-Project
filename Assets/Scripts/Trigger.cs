1443da78273c7f7201af3dbabcb65f937b8e9417
Lucas Sharkey-Chouinard <lucas@theshards.ca>
2023-11-22 11:33:36 -0400

    Initial commit

---
+++ Assets/Scripts/Trigger.cs
@@ -0,0 +1,20 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.Playables;
+
+public class Trigger : MonoBehaviour
+{
+    
+    public PlayableDirector timeline;
+
+    private void OnTriggerEnter(Collider other)
+    {
+        if (other.gameObject.tag == "Player")
+        {
+            timeline.Play();
+            Debug.Log("Played Timeline");
+        }
+    }
+
+}
